bd76ffe5025c9c6746210863b31ef49d30d33f64
Ian Chen <iancodinghtml@gmail.com>
2020-08-24 22:48:16 +0800

    [UPDATE] home.jsx&&projects.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import styles from './card.scss'
 const Home = class Home extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      data: 'x',
-      names: null
+    this.state={
+      people: [
+        {
+          name: 'IAN',
+          exp: 'SITCON 2020'
+        },
+        {
+          name: 'EC',
+          exp: ''
+        }
+      ]
     }
   }
   render() {
+    let ref = this.state.people;
+    let people = ref.map(items=> {return(
+    <li>{items.name}</li>
+    )});
     return(
     <div>
+      <div className={styles.container}>
       <h1>Wellcome to DG Lab.</h1>
-      {/* {this.state.names} */}
-      <button onClick={()=>{console.log(this.props)}}>Submit</button>
+      <h2>Co founder</h2>
+        {people}
+      </div>
     </div>
     );
   }
