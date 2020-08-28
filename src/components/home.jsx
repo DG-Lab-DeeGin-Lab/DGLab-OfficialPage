b870c21ab2addbc45f84cce48480bb019ef114cd
ianchen0119 <t106360734@ntut.edu.tw>
2020-08-29 01:25:23 +0800

    [UPDATE] Home && Projects page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,44 +1,48 @@
 import React from "react";
-import styles from './card.scss'
+import styles from "./card.scss";
 const Home = class Home extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       people: [
         {
-          id:1,
-          pictuere_url:'https://avatars3.githubusercontent.com/u/42661015?s=460&u=06eab7d7740496192f081d9538a2f1b7c877d1f7&v=4',
-          name: 'IAN',
-          exp: 'Speaker @ SITCON 2020'
+          id: 1,
+          pictuere_url:
+            "https://avatars3.githubusercontent.com/u/42661015?s=460&u=06eab7d7740496192f081d9538a2f1b7c877d1f7&v=4",
+          name: "IAN",
+          exp: "Speaker @ SITCON 2020",
         },
         {
-          id:2,
-          pictuere_url:'https://avatars1.githubusercontent.com/u/23449425?s=460&u=fbbcb5368ee41f82174918ee434be6e1200e5610&v=4',
-          name: 'EC',
-          exp: '黑蘋果愛好者'
-        }
-      ]
-    }
+          id: 2,
+          pictuere_url:
+            "https://avatars1.githubusercontent.com/u/23449425?s=460&u=fbbcb5368ee41f82174918ee434be6e1200e5610&v=4",
+          name: "EC",
+          exp: "黑蘋果愛好者",
+        },
+      ],
+    };
   }
   render() {
     let ref = this.state.people;
-    let people = ref.map(items=> {return(
-    <div key={items.id} className={styles.card}>
-      <div className={styles.card__picFrame}>
-        <img className={styles.card__pic} src={items.pictuere_url} alt=""/>
+    let people = ref.map((items) => {
+      return (
+        <div key={items.id} className={styles.card}>
+          <div className={styles.card__picFrame}>
+            <img className={styles.card__pic} src={items.pictuere_url} alt="" />
+          </div>
+          <div>{items.name}</div>
+          <li>{items.exp}</li>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <div className={styles.container}>
+          <h1>Wellcome to DG Lab.</h1>
+          <h1 className={styles.focus}>Co founder</h1>
+          {people}
+        </div>
       </div>
-      <div>{items.name}</div>
-      <li>{items.exp}</li>
-    </div>
-    )});
-    return(
-    <div>
-      <div className={styles.container}>
-      <h1>Wellcome to DG Lab.</h1>
-      <h2 className={styles.focus}>Co founder</h2>
-        {people}
-      </div>
-    </div>
     );
   }
 };
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,38 +1,43 @@
-import React from 'react';
-import styles from './card.scss';
-const Projects = class Projects extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = 
+import React from "react";
+import styles from "./card.scss";
+const Projects = class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: [
         {
-            project: [
-                {
-                    id:1,
-                    name: 'BPSS',
-                    description: 'nothing'
-                },
-                {
-                    id:2,
-                    name: 'Chatting Room',
-                    description: 'nothing'
-                }
-            ]
-        }
-    }
-    render(){
-        let ref = this.state.project;
-        let card = ref.map((items)=>{
-        return (
+          id: 1,
+          name: "BPSS",
+          description:
+            "使用 HyperLedger-Fabric 作為區塊鏈網路主體。並搭配 Vue.js, Koa.js, django 建置全端應用。",
+          source: false,
+        },
+        {
+          id: 2,
+          name: "Chatting Room",
+          description:
+            "基於 Socket.io, Express.js, Vue.js 開發的匿名配對聊天室。",
+          source: true,
+        },
+      ],
+    };
+  }
+  render() {
+    let ref = this.state.project;
+    let card = ref.map((items) => {
+      return (
         <div key={items.id} className={styles.card}>
-        <div key={items.name}>{items.name}</div>
+          <h2 key={items.name}>{items.name}</h2>
+          <div key={items.description}>{items.description}</div>
+          {items.source ? (
+            <div className={styles.label + " " + styles.open}>open</div>
+          ) : (
+            <div className={styles.label + " " + styles.closed}>closed</div>
+          )}
         </div>
-        )
-        })
-        return(
-            <div className={styles.container}>
-                {card}
-            </div>
-        )
-    }
-}
+      );
+    });
+    return <div className={styles.container}>{card}</div>;
+  }
+};
 export default Projects;
